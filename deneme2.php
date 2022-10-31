4fa1c6c134a8490d2efdbba86c2bd030ba09e623
Efe Onkar <66698996+lRadioactivee@users.noreply.github.com>
2022-11-01 02:06:05 +0300

    Add files via upload

---
+++ deneme2.php
@@ -0,0 +1,26 @@
+<?php
+
+$girdiler = array("agilSayisi" => "5" , "agilKapasitesi" => "30", "koyunSayisi" => "73");
+$kapasite = $girdiler["agilSayisi"] * $girdiler["agilKapasitesi"];
+$kalanKontrol = $girdiler["koyunSayisi"] - ($girdiler["agilKapasitesi"] * $girdiler["agilSayisi"]);
+
+echo "Toplam Ağıl:" .$girdiler["agilSayisi"] ."<br>";
+echo "Toplam Kapasite:" .$kapasite ."<br>";
+echo "Koyun Sayısı:" .$girdiler["koyunSayisi"] . "<br>";
+
+for ($girdiler["agilSayisi"]; $girdiler["agilSayisi"]>=1; $girdiler["agilSayisi"] = $girdiler["agilSayisi"] - 1){
+    if ($girdiler["koyunSayisi"] >= $girdiler["agilKapasitesi"] ){
+        echo $girdiler["agilSayisi"]. ". Ağıl:" . $girdiler["agilKapasitesi"] . "<br>";
+    }
+    elseif ($girdiler["koyunSayisi"] < $girdiler["agilKapasitesi"] && $girdiler["koyunSayisi"]>0) {
+        echo $girdiler["agilSayisi"]. ". Ağıl:" . $girdiler ["koyunSayisi"] . "<br>";
+    }
+    elseif ($girdiler["koyunSayisi"] < 0) {
+        echo $girdiler["agilSayisi"]. ". Ağıl:" . "0" . "<br>";
+    }
+$girdiler["koyunSayisi"] = $girdiler["koyunSayisi"] - $girdiler["agilKapasitesi"];
+}
+
+if ($kalanKontrol > 0) {
+    echo "<br><br>" . "Dışarıda Kalan:" .$kalanKontrol ."Koyun vardır.";
+}
