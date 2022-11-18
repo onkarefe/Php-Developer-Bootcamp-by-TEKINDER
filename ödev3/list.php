0fdebb8c369af6795eddbc04a1332ea3af7c481a
Efe Onkar <66698996+onkarefe@users.noreply.github.com>
2022-11-19 01:21:34 +0300

    Add files via upload

---
+++ ödev3/list.php
@@ -0,0 +1,28 @@
+
+<?php
+$baglan = new PDO("mysql:host=localhost;dbname=uygulama;charset=utf8","atlas","1234");
+$baglan -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$veri = $baglan->query("select * from girdiler",PDO::FETCH_ASSOC);
+echo "<table border ='1' width = '%100'>
+    <tr>
+    <td><b>ADI SOYADI</td>
+    <td><b>TELEFON NUMARASI</b></td>
+    <td><b>İşlem</b></td>";
+foreach ($veri as $satir){
+
+    echo "<tr>
+    <td><b>$satir[adsoyad]</td>
+    <td>$satir[telno]</b></td>
+    <td><html><form method = 'post' action = 'kayitsil.php'>
+    <input type = 'submit' value = 'Sil'></b></td>";
+}
+echo "</form>";
+
+echo "<table><tr>Toplamda $satir[id] adet kayıt vardır</table>";
+
+?>
+<body>
+<form  method = "post" action ="index.php">
+<br>
+<input type = "submit" value="Kayıt Ekranına Dönmek için Lütfen Tılayınız">
+</form>
