4616e2df4b1764dc801226cd753a7ab4f8635d63
Efe Onkar <66698996+onkarefe@users.noreply.github.com>
2022-12-03 14:53:59 +0300

    Delete proje directory

--- proje/baglan.php
+++
@@ -1,6 +0,0 @@
-<?php
-$baglan=new PDO("mysql:host=localhost;dbname=emlak;charset=utf8","atlas","1234");
-$baglan->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$baglan->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-$baglan->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
-$baglan->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
--- proje/cikis.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo "<script>
-   window.top.location.href = 'login.php';
-   </script>";
